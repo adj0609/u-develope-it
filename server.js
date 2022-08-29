c869214e855221e8543c706af48c9a2ae04f46c5
Andrea Jared <adj0803@gmail.com>
2022-08-29 16:58:18 -0400

    commit

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-const express = require ('express');
+const mysql = require('mysql2');
+const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-app.use(express.urlencoded({ extended: false}));
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '12Cribben!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
+
+
+
+// Default response for any other request (Not Found) must be last
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
