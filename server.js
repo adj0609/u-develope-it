8552ce18c1c7e40c7d7260634da477193d8ec6ee
Andrea Jared <adj0803@gmail.com>
2022-08-29 19:49:34 -0400

    add create delete

--- server.js
+++ server.js
@@ -19,11 +19,35 @@ const db = mysql2.createConnection(
     },
     console.log('Connected to the election database.')
   );
-
+// get all candidates
   db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
   });
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+  });
+ // Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+  // Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
 
+db.query(sql, params, (err, result) => {
+if (err) {
+console.log(err);
+}
+console.log(result);
+});
 
 app.use((req, res) => {
     res.status(404).end();
